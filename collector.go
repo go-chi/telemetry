c4514871562fed51630be741890ba14bc408c3ac
Peter Kieltyka <peter.kieltyka@gmail.com>
2022-04-03 14:38:43 -0400

    collector http handler, accept list of path filters

--- collector.go
+++ collector.go
@@ -10,11 +10,20 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-// Collector creates a handler that exposes a /metrics endpoint
-func Collector(cfg Config) func(next http.Handler) http.Handler {
+// Collector creates a handler that exposes a /metrics endpoint. Passing
+// an array of strings to pathPrefixFilters will help reduce the noise on the service
+// from random Internet traffic; that is, only the path prefixes will be measured.
+func Collector(cfg Config, optPathPrefixFilters ...[]string) func(next http.Handler) http.Handler {
 	if (!cfg.AllowAny && !cfg.AllowInternal) && (cfg.Username == "" || cfg.Password == "") {
 		return func(next http.Handler) http.Handler {
 			return next
+		}
+	}
+
+	pathPrefixFilters := []string{}
+	if len(optPathPrefixFilters) > 0 {
+		for _, v := range optPathPrefixFilters[0] {
+			pathPrefixFilters = append(pathPrefixFilters, v)
 		}
 	}
 
@@ -62,6 +71,22 @@ func Collector(cfg Config) func(next http.Handler) http.Handler {
 				// serve metrics page
 				metricsHandler.Handler(next).ServeHTTP(w, r)
 				return
+			}
+
+			// check path filters
+			if len(pathPrefixFilters) > 0 {
+				found := false
+				for _, p := range pathPrefixFilters {
+					if strings.EqualFold(r.URL.Path, p) {
+						found = true
+						break
+					}
+				}
+				if !found && r.URL.Path != "/" {
+					// skip measurement of the http request
+					next.ServeHTTP(w, r)
+					return
+				}
 			}
 
 			// measure request
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.16
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/go-chi/chi/v5 v5.0.3
+	github.com/go-chi/chi/v5 v5.0.7
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/m3db/prometheus_client_golang v0.8.1 // indirect
 	github.com/m3db/prometheus_client_model v0.1.0 // indirect
@@ -13,7 +13,8 @@ require (
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
-	github.com/twmb/murmur3 v1.1.5 // indirect
-	github.com/uber-go/tally v3.4.1+incompatible
-	go.uber.org/atomic v1.8.0 // indirect
+	github.com/twmb/murmur3 v1.1.6 // indirect
+	github.com/uber-go/tally v3.4.3+incompatible
+	go.uber.org/atomic v1.9.0 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
